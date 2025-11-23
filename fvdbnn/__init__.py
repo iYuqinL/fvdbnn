979757371c908b0ec23344767310d2c95755538b
Yuqin Liang <YuqinLiangX@gmail.com>
2025-11-23 17:19:25 +0800

    feat(utils): add basic utilities

---
+++ fvdbnn/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+###
+# File: __init__.py
+# Created Date: Saturday, November 22nd 2025, 12:17:30 am
+# Author: iYuqinL
+# -----
+# Last Modified: 
+# Modified By: 
+# -----
+# Copyright © 2025 iYuqinL Holding Limited
+# 
+# All shall be well and all shall be well and all manner of things shall be well.
+# Nope...we're doomed!
+# -----
+# HISTORY:
+# Date      	By	Comments
+# ----------	---	----------------------------------------------------------
+###
